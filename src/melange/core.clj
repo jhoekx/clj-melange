51e0fde12846e1eadd6da3ffb1911845373cc290
Jeroen Hoekx <jeroen@hoekx.be>
2016-04-02 14:01:42 +0200

    Refer to things by ID

--- src/melange/core.clj
+++ src/melange/core.clj
@@ -5,64 +5,65 @@
 
 (defmulti handle-event (fn [_ [event _]] event))
 
-(defn- make-thing [name]
-  {:name name
+(defn- make-thing [id name]
+  {:id   id
+   :name name
    :vars {}})
 
-(defn- add-thing [state [thing-type name]]
-  (->> (make-thing name)
+(defn- add-thing [state [thing-type id name]]
+  (->> (make-thing id name)
        (conj (thing-type state))
        (assoc state thing-type)))
 
-(defn- remove-thing [state [thing-type name]]
+(defn- remove-thing [state [thing-type id]]
   (->> (thing-type state)
-       (filter #(not= name (:name %)))
+       (filter #(not= id (:id %)))
        (assoc state thing-type)))
 
-(defn- change-thing [name f]
+(defn- change-thing [id f]
   (fn [thing]
-    (if (= name (:name thing))
+    (if (= id (:id thing))
       (f thing)
       thing)))
 
-(defn- add-variable-to-thing [state [thing-type name key value]]
+(defn- add-variable-to-thing [state [thing-type id key value]]
   (->> (thing-type state)
-       (map (change-thing name #(assoc-in % [:vars key] value)))
+       (map (change-thing id #(assoc-in % [:vars key] value)))
        (assoc state thing-type)))
 
-(defn- remove-variable-from-thing [state [thing-type name key]]
+(defn- remove-variable-from-thing [state [thing-type id key]]
   (->> (thing-type state)
-       (map (change-thing name #(assoc % :vars (dissoc (:vars %) key))))
+       (map (change-thing id #(assoc % :vars (dissoc (:vars %) key))))
        (assoc state thing-type)))
 
 (defmethod handle-event :tag-added
-  [state [_ {:keys [name]}]]
-  (add-thing state [:tags name]))
+  [state [_ {:keys [id name]}]]
+  (add-thing state [:tags id name]))
 
 (defmethod handle-event :tag-removed
-  [state [_ {:keys [name]}]]
-  (remove-thing state [:tags name]))
+  [state [_ {:keys [id]}]]
+  (remove-thing state [:tags id]))
 
 (defmethod handle-event :variable-added-to-tag
-  [state [_ {:keys [name key value]}]]
-  (add-variable-to-thing state [:tags name key value]))
+  [state [_ {:keys [id key value]}]]
+  (add-variable-to-thing state [:tags id key value]))
 
 (defmethod handle-event :variable-removed-from-tag
-  [state [_ {:keys [name key]}]]
-  (remove-variable-from-thing state [:tags name key]))
+  [state [_ {:keys [id key]}]]
+  (remove-variable-from-thing state [:tags id key]))
 
 (defmethod handle-event :item-added
-  [state [_ {:keys [name]}]]
-  (add-thing state [:items name]))
+  [state [_ {:keys [id name]}]]
+  (add-thing state [:items id name]))
 
 (defmethod handle-event :item-removed
-  [state [_ {:keys [name]}]]
-  (remove-thing state [:items name]))
+  [state [_ {:keys [id]}]]
+  (remove-thing state [:items id]))
 
 (defmethod handle-event :variable-added-to-item
-  [state [_ {:keys [name key value]}]]
-  (add-variable-to-thing state [:items name key value]))
+  [state [_ {:keys [id key value]}]]
+  (add-variable-to-thing state [:items id key value]))
 
 (defmethod handle-event :variable-removed-from-item
-  [state [_ {:keys [name key]}]]
-  (remove-variable-from-thing state [:items name key]))
+  [state [_ {:keys [id key]}]]
+  (remove-variable-from-thing state [:items id key]))
--- test/melange/core_test.clj
+++ test/melange/core_test.clj
@@ -3,9 +3,9 @@
             [melange.core :refer :all]))
 
 (deftest tags
-  (let [one-tag (handle-event default-state [:tag-added {:name "tag1"}])
-        second-tag (handle-event one-tag [:tag-added {:name "tag2"}])
-        removed-tag (handle-event second-tag [:tag-removed {:name "tag1"}])]
+  (let [one-tag (handle-event default-state [:tag-added {:id "1" :name "tag1"}])
+        second-tag (handle-event one-tag [:tag-added {:id "2" :name "tag2"}])
+        removed-tag (handle-event second-tag [:tag-removed {:id "1"}])]
     (testing "Add one tag"
       (is (= "tag1" (-> one-tag :tags first :name))))
     (testing "Add a second tag"
@@ -15,34 +15,34 @@
       (is (= 1 (-> removed-tag :tags count)))
       (is (= "tag2" (-> removed-tag :tags first :name))))))
 
-(defn- find-tag [name state]
+(defn- find-tag-by-name [name state]
   (->> (:tags state)
        (filter #(= name (:name %)))
        first))
 
 (deftest tag-variables
-  (let [tag (handle-event default-state [:tag-added {:name "tag"}])
-        text-var (handle-event tag [:variable-added-to-tag {:name  "tag"
+  (let [tag (handle-event default-state [:tag-added {:id "1" :name "tag"}])
+        text-var (handle-event tag [:variable-added-to-tag {:id    "1"
                                                             :key   "text"
                                                             :value "blah"}])
-        list-var (handle-event text-var [:variable-added-to-tag {:name  "tag"
+        list-var (handle-event text-var [:variable-added-to-tag {:id    "1"
                                                                  :key   "list"
                                                                  :value [1 2 3]}])
-        removed-var (handle-event list-var [:variable-removed-from-tag {:name "tag"
-                                                                        :key  "text"}])]
+        removed-var (handle-event list-var [:variable-removed-from-tag {:id  "1"
+                                                                        :key "text"}])]
     (testing "Add a variable"
-      (is (= "blah" (-> (find-tag "tag" text-var) :vars (get "text")))))
+      (is (= "blah" (-> (find-tag-by-name "tag" text-var) :vars (get "text")))))
     (testing "Add a second variable"
-      (is (= 2 (-> (find-tag "tag" list-var) :vars count)))
-      (is (= [1 2 3] (-> (find-tag "tag" list-var) :vars (get "list")))))
+      (is (= 2 (-> (find-tag-by-name "tag" list-var) :vars count)))
+      (is (= [1 2 3] (-> (find-tag-by-name "tag" list-var) :vars (get "list")))))
     (testing "Remove a variable"
-      (is (= 1 (-> (find-tag "tag" removed-var) :vars count)))
-      (is (= [1 2 3] (-> (find-tag "tag" removed-var) :vars (get "list")))))))
+      (is (= 1 (-> (find-tag-by-name "tag" removed-var) :vars count)))
+      (is (= [1 2 3] (-> (find-tag-by-name "tag" removed-var) :vars (get "list")))))))
 
 (deftest items
-  (let [one-item (handle-event default-state [:item-added {:name "item1"}])
-        two-items (handle-event one-item [:item-added {:name "item2"}])
-        removed-item (handle-event two-items [:item-removed {:name "item2"}])]
+  (let [one-item (handle-event default-state [:item-added {:id "1" :name "item1"}])
+        two-items (handle-event one-item [:item-added {:id "2" :name "item2"}])
+        removed-item (handle-event two-items [:item-removed {:id "1"}])]
     (testing "Add an item"
       (is (= "item1" (-> one-item :items first :name))))
     (testing "Adding a second item"
@@ -51,26 +51,26 @@
     (testing "Removing an item"
       (is (= 1 (-> removed-item :items count))))))
 
-(defn- find-item [name state]
+(defn- find-item-by-name [name state]
   (->> (:items state)
        (filter #(= name (:name %)))
        first))
 
 (deftest item-variables
-  (let [item (handle-event default-state [:item-added {:name "item"}])
-        text-var (handle-event item [:variable-added-to-item {:name  "item"
+  (let [item (handle-event default-state [:item-added {:id "1" :name "item"}])
+        text-var (handle-event item [:variable-added-to-item {:id    "1"
                                                               :key   "text"
                                                               :value "blah"}])
-        list-var (handle-event text-var [:variable-added-to-item {:name  "item"
+        list-var (handle-event text-var [:variable-added-to-item {:id    "1"
                                                                   :key   "list"
                                                                   :value [1 2 3]}])
-        removed-var (handle-event list-var [:variable-removed-from-item {:name "item"
-                                                                         :key  "text"}])]
+        removed-var (handle-event list-var [:variable-removed-from-item {:id  "1"
+                                                                         :key "text"}])]
     (testing "Add a variable"
-      (is (= "blah" (-> (find-item "item" text-var) :vars (get "text")))))
+      (is (= "blah" (-> (find-item-by-name "item" text-var) :vars (get "text")))))
     (testing "Add a second variable"
-      (is (= 2 (-> (find-item "item" list-var) :vars count)))
-      (is (= [1 2 3] (-> (find-item "item" list-var) :vars (get "list")))))
+      (is (= 2 (-> (find-item-by-name "item" list-var) :vars count)))
+      (is (= [1 2 3] (-> (find-item-by-name "item" list-var) :vars (get "list")))))
     (testing "Remove a variable"
-      (is (= 1 (-> (find-item "item" removed-var) :vars count)))
-      (is (= [1 2 3] (-> (find-item "item" removed-var) :vars (get "list")))))))
+      (is (= 1 (-> (find-item-by-name "item" removed-var) :vars count)))
+      (is (= [1 2 3] (-> (find-item-by-name "item" removed-var) :vars (get "list")))))))
