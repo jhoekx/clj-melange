fb0dce54f2a82f7c8ca16380a2423f368eaede30
Jeroen Hoekx <jeroen@hoekx.be>
2016-05-07 20:19:27 +0200

    Add lein auto plugin to run tests automatically

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A lightweigt CMDB"
   :url "http://github.com/jhoekx/melange"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
+                   :plugins      [[lein-auto "0.1.2"]]}})
